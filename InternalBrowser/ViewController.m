b42289dcab5979bb86d334c9eeb59b8b6f94733b
ideal <lxyang6688@163.com>
2016-07-07 15:12:18 +0800

    添加网页提供者
    1.添加网页显示Label。
    2.网页加载成功后显示该Label。

--- InternalBrowser/ViewController.m
+++ InternalBrowser/ViewController.m
@@ -9,13 +9,17 @@
 #import "ViewController.h"
 #import <WebKit/WKWebView.h>
 
+#define kUrl @"https://www.cmall.com/page/CN/provision.html"//https://www.cmall.com/page/CN/activity/Iron_Lady.html?clientType=ios"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) NSURL *url;
 
 @property (strong, nonatomic) WKWebView *webView;
 @property (strong, nonatomic) UIView *progressView;
 @property (strong, nonatomic) CALayer *progressLayer;
 
+@property (strong, nonatomic) UILabel *hostLabel;
 
 @end
 
@@ -24,14 +28,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.hostLabel];
     [self.view addSubview:self.webView];
-    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
-    
     [self.view addSubview:self.progressView];
-    
     [self.progressView.layer addSublayer:self.progressLayer];
     
-    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://www.cmall.com/page/CN/activity/Iron_Lady.html?clientType=ios"]];
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+    self.url = [NSURL URLWithString:kUrl];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:self.url];
     [self.webView loadRequest:request];
 }
 
@@ -61,6 +66,7 @@
             
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 self.progressLayer.frame = CGRectMake(0, 0, 0, 3);
+                self.hostLabel.text = [NSString stringWithFormat:@"网页由 %@ 提供", self.url.host];
             });
         }
     }
@@ -70,7 +76,9 @@
 
 - (WKWebView *)webView{
     if (!_webView) {
-        _webView = [[WKWebView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        _webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+        _webView.scrollView.backgroundColor = [UIColor clearColor];
+        [_webView.scrollView setContentInset:UIEdgeInsetsMake(64, 0, 0, 0)];
     }
     return _webView;
 }
@@ -90,6 +98,15 @@
         _progressLayer.backgroundColor = [UIColor redColor].CGColor;
     }
     return _progressLayer;
+}
+
+- (UILabel *)hostLabel{
+    if (!_hostLabel) {
+        _hostLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 30)];
+        _hostLabel.textAlignment = NSTextAlignmentCenter;
+        _hostLabel.font = [UIFont systemFontOfSize:14];
+    }
+    return _hostLabel;
 }
 
 @end
