1487529b5e429cccd03eeb0bb88fc46f97abe299
ideal <lxyang6688@163.com>
2016-07-07 13:32:53 +0800

    添加基本浏览器界面
    1.初始化工程。
    2.引入IOS8.0新特性：WKWebView。
    3.添加进度条。
    4.设置成导航控制器管理界面。

--- InternalBrowser/ViewController.m
+++ InternalBrowser/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WKWebView.h>
+
 
 @interface ViewController ()
+
+@property (strong, nonatomic) WKWebView *webView;
+@property (strong, nonatomic) UIView *progressView;
+@property (strong, nonatomic) CALayer *progressLayer;
+
 
 @end
 
@@ -17,11 +24,72 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.webView];
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+    [self.view addSubview:self.progressView];
+    
+    [self.progressView.layer addSublayer:self.progressLayer];
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://www.cmall.com/page/CN/activity/Iron_Lady.html?clientType=ios"]];
+    [self.webView loadRequest:request];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc{
+    [(WKWebView *)self.view removeObserver:self forKeyPath:@"estimatedProgress"];
+}
+
+#pragma mark -
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        self.progressLayer.opacity = 1;
+        if ([change[@"new"] floatValue] < [change[@"old"] floatValue]) {
+            return;
+        }
+        
+        self.progressLayer.frame = CGRectMake(0, 0, self.view.bounds.size.width * [change[@"new"] floatValue], 3);
+        if ([change[@"new"] floatValue] == 1) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progressLayer.opacity = 0;
+            });
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progressLayer.frame = CGRectMake(0, 0, 0, 3);
+            });
+        }
+    }
+}
+
+#pragma mark -
+
+- (WKWebView *)webView{
+    if (!_webView) {
+        _webView = [[WKWebView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    }
+    return _webView;
+}
+
+- (UIView *)progressView{
+    if (!_progressView) {
+        _progressView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 3)];
+        _progressView.backgroundColor = [UIColor clearColor];
+    }
+    return _progressView;
+}
+
+- (CALayer *)progressLayer{
+    if (!_progressLayer) {
+        _progressLayer = [CALayer layer];
+        _progressLayer.frame = CGRectMake(0, 0, 0, 3);
+        _progressLayer.backgroundColor = [UIColor redColor].CGColor;
+    }
+    return _progressLayer;
 }
 
 @end
